9219322ffd6f98cfbe454257d30ab093fcbf6243
bitmage <brandonjmason@gmail.com>
2013-10-18 01:25:27 -0700

    added correct implementation

--- app/server.js
+++ app/server.js
@@ -1,9 +1,48 @@
+require('./user');
+
 var express = require('express'),
-  mongoose = require('mongoose'),
+  User = require('mongoose').models.User,
   app = express();
 
-app.get('/', function(req, res) {
-  return res.send('Hello World');
+app.use(express.bodyParser());
+
+// list
+app.get('/users', function(req, res) {
+  User.find({}, function(err, users){
+    res.send({users: users});
+  });
+});
+
+// show
+app.get('/users/:id', function(req, res) {
+  id = req.params.id;
+  User.findById(id, function(err, user){
+    res.send({user: user});
+  });
+});
+
+// create
+app.post('/users', function(req, res) {
+  User.create(req.body, function(err, user) {
+    res.send(201, {user: user});
+  });
+});
+
+// update
+app.put('/users/:id', function(req, res) {
+  id = req.params.id;
+  User.findOneAndUpdate({_id: id}, req.body, function(err, user) {
+    res.send(200, {user: user});
+  });
+});
+
+// destroy
+app.delete('/users/:id', function(req, res) {
+  id = req.params.id;
+  User.remove({_id: id}, function() {
+    res.send({});
+  });
 });
 
 app.listen(3333);
+console.log('Server running at: http://localhost:' + 3333);
--- app/user.js
+++ app/user.js
@@ -1,1 +1,12 @@
 var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/crud-intro');
+
+mongoose.model('User', new mongoose.Schema({
+  email: String,
+  first_name: String,
+  last_name: String,
+  role: String
+}));
+
+return mongoose.model('User');
